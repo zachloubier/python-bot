fc8b920ba898607f4ef935761215b817cc1f0483
zachloubier <zach@betabrand.com>
2017-04-17 20:52:09 -0700

    Fix environment variable get

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def receive_message():
-	if request.args.get('hub_mode') == 'subscribe' and request.args.get('hub.challenge'):
-		if not request.args.get('hub.verify_token') == os.environ['VERIFY_TOKEN']:
+def verify():
+	if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.challenge'):
+		if not request.args.get('hub.verify_token') == os.environ.get('VERIFY_TOKEN'):
 			return 'Verification token mismatch', 403
 		return request.args.get('hub.challenge'), 200
 
