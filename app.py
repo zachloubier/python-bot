fb7b1c975bff0443147d575df1f238bb0ac6009e
zachloubier <zach@betabrand.com>
2017-04-17 20:26:41 -0700

    Testing heroku

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
-page_token = 'EAAF3iaUxWAIBAMJ06XDwFZA0gZCQkTZC0cqudDzEUZBxLcezyfPC2F0Mz1SEaLSjb0guZCrtoExae0pQj7BqdoMqS060RkcdyG3VVgJ80Tjnl84xtER7WIKXLjie4udr7WpSyIG6uYh43t8sd8E678PQnHZC3evn1OpzKPlgvZCAQZDZD'
-app_id = '412908042409986'
+# page_token = 'EAAF3iaUxWAIBAMJ06XDwFZA0gZCQkTZC0cqudDzEUZBxLcezyfPC2F0Mz1SEaLSjb0guZCrtoExae0pQj7BqdoMqS060RkcdyG3VVgJ80Tjnl84xtER7WIKXLjie4udr7WpSyIG6uYh43t8sd8E678PQnHZC3evn1OpzKPlgvZCAQZDZD'
+# app_id = '412908042409986'
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def receive_message():
-	return 'Got your message!'
+	return 'Got your message!', 200
